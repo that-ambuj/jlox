f98f3339cbe56ebe687cfabb1ef111bfd039faac
that-ambuj <ambujs89@gmail.com>
2023-03-29 12:52:25 +0530

    Initial Commit

---
+++ app/src/main/java/jlox/Lox.java
@@ -0,0 +1,92 @@
+package jlox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import jlox.Scanner;
+
+public class Lox {
+    static boolean hadError = false;
+
+    public static void main(String[] args) {
+        try {
+            if (args.length > 1) {
+                System.out.println("Usage: jlox [script]");
+                System.exit(64);
+            } else if (args.length == 1) {
+                runFile(args[0]);
+            } else {
+                runPrompt();
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.err.println(e);
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError)
+            System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.println("> ");
+            String line = reader.readLine();
+            if (line == null)
+                break;
+            run(line);
+
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ":" + message);
+        hadError = true;
+    }
+}
+
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
